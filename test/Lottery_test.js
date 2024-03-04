0848b1345e39f47bc9b22eb843fc647457999ab9
“오승현” <timber3@naver.com>
2024-03-05 01:15:21 +0900

    03.05 studying

--- test/Lottery_test.js
+++ test/Lottery_test.js
@@ -1,10 +1,11 @@
 const Lottery = artifacts.require("Lottery");
 const AssertRevert = require('./AssertRevert');
+const ExpectEvent = require('./ExpectEvent');
 
 contract('Lottery', function ([deployer, user1, user2]) {
     let lottery;
     beforeEach(async () => {
-        console.log('BeforeEach');
+        console.log('==========================================================');
         lottery = await Lottery.new();
     })
 
@@ -14,16 +15,36 @@ contract('Lottery', function ([deployer, user1, user2]) {
     })
 
     describe('Bet', function () {
-        it.only('should fail when the bet money is not 0.005 ETH', async () => {
+        it('should fail when the bet money is not 0.005 ETH', async () => {
             // smart contract 를 만들때는 트랜잭션 오브젝트를 만들 수 있다.  5 * 10 * 15
             await AssertRevert(lottery.bet('0xab', { from: user1, value: 5 * 10 ** 15 }));
         })
-        it('should put the bet queue with 1 bet', async () => {
+        it.only('should put the bet queue with 1 bet', async () => {
 
             // bet
+            let receipt = await lottery.bet('0xab', { from: user1, value: 5 * (10 ** 15) });
+            console.log(receipt);
 
-            // check contract balance
+            // check pot
+            let pot = await lottery.getPot();
+            assert.equal(pot, 0);
+
+            // check contract balance ( 잔고 )
+            let contractBalance = await web3.eth.getBalance(lottery.address);
+            assert.equal(contractBalance, (5 * 10 ** 15));
+
+            // check betInfo
+            let currentBlockNumber = await web3.eth.getBlockNumber();
+            let bet = await lottery.getBetInfo(0);
+
+            assert(bet.answerBlockNumber, currentBlockNumber + 3);
+            assert(bet.bettor, user1);
+            assert(bet.challenges, '0xab');
+
+            // check Logs
+            await ExpectEvent.inLogs(receipt.logs, 'BET');
         })
+
     })
 
 })
